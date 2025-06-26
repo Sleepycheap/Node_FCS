ba39f2b1d81b823ae976d384dc1ff7646e60bcdd
Sleepycheap <anthony.vaut@gmail.com>
2025-06-26 11:01:32 -0500

    Fixed .env ignore

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,25 +1,25 @@
-const crypto = require("crypto");
-const catchAsync = require("./../utils/catchAsync");
-const AppError = require("./../utils/appError");
-const axios = require("axios");
-const dotenv = require("dotenv");
+const crypto = require('crypto');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+const axios = require('axios');
+const dotenv = require('dotenv');
 
-dotenv.config({ path: "./config.ev" });
+dotenv.config({ path: './.env' });
 
 const tokenEndpoint =
-  "https://login.microsoftonline.com/e5f81c16-7ed3-48ef-98dc-02b2d0ea9a35/oauth2/v2.0/token";
+  'https://login.microsoftonline.com/e5f81c16-7ed3-48ef-98dc-02b2d0ea9a35/oauth2/v2.0/token';
 
 async function getAccessToken() {
   const params = new URLSearchParams();
-  params.append("grant_type", process.env.GRANT_TYPE);
-  params.append("client_id", process.env.CLIENT_ID);
-  params.append("client_secret", process.env.CLIENT_SECRET);
-  params.append("scope", process.env.SCOPE);
+  params.append('grant_type', process.env.GRANT_TYPE);
+  params.append('client_id', process.env.CLIENT_ID);
+  params.append('client_secret', process.env.CLIENT_SECRET);
+  params.append('scope', process.env.SCOPE);
 
   try {
     const res = await axios.post(tokenEndpoint, params, {
       headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
+        'Content-Type': 'application/x-www-form-urlencoded',
       },
     });
 
